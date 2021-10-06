83f6c8385eaf94c4367a1394420151c7e7cd84af
DamianoLandi <damianolandi01@gmail.com>
2021-10-06 17:40:03 +0200

    Display e Sconto

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ class Product{
         return $this->availability;
     }
 
-    protected function __construct($_name, $_price, $_weigth, $_availability){
+    public function __construct($_name, $_price, $_weigth, $_availability){
         $this->name = $_name;
         $this->price = $_price;
         $this->weigth = $_weigth;
@@ -36,13 +36,15 @@ class Book extends Product{
     protected $pages;
     protected $genre;
 
-    protected function __construct($_name, $_price, $_weigth, $_availability, $_author, $_isbn, $_pages, $_genre){
+    public function __construct($_name, $_price, $_weigth, $_availability, $_author, $_isbn, $_pages, $_genre){
         $this->author = $_author;
         $this->isbn = $_isbn;
         $this->pages = $_pages;
         $this->genre = $_genre;
-
-        parent::__construct($_name, $_price, $_weigth, $_availability);
+        $this->name = $_name;
+        $this->price = $_price;
+        $this->weigth = $_weigth;
+        $this->availability = $_availability;
     }
 
     public function getAuthor(){
@@ -79,7 +81,7 @@ class User{
         return $this->age;
     }
 
-    protected function __construct($_name, $_surname, $_age){
+    public function __construct($_name, $_surname, $_age){
         $this->name = $_name;
         $this->surname = $_surname;
         $this->age = $_age;
@@ -90,19 +92,20 @@ class Premium extends User{
     protected $level;
     protected $discount;
 
-    protected function __construct($_name, $_surname, $_age, $_level, $_discount){
+    public function __construct($_name, $_surname, $_age, $_level, $_discount = 0){
         $this->level = $_level;
         $this->discount = $_discount;
-
-        parent::__construct($_name, $_surname, $_age);
+        $this->name = $_name;
+        $this->surname = $_surname;
+        $this->age = $_age;
     }
 
     public function getDiscount(){
-        if ($this->level == 1){
+        if ($this->level == '1'){
             $this->discount = 20;
-        }elseif ($this->level == 2){
+        }elseif ($this->level == '2'){
             $this->discount = 30;
-        }elseif($this->level == 3){
+        }elseif($this->level == '3'){
             $this->discount = 50;
         }else{
             $this->discount = "Il livello utente non è valido";
@@ -111,9 +114,26 @@ class Premium extends User{
         return $this->discount;
     }
 
-}
+};
 
+$user = new Premium("Mario", "Rossi", "25", "2");
+$book = new Book("Eragon", 25, "0.5", "18", "Franco", "1234567890123", "569", "Fantasy");
 
+$discounted_book = $book->getPrice() / 100 * (100 - $user->getDiscount());
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Il prezzo standard è <?php echo $book->getPrice() ?>$</p>
+    <p>Il prezzo per l'utente <?php echo $user->getName() . " " . $user->getSurname()?> è <?php echo $discounted_book?>$</p>
+</body>
+</html>
